a6974cbee0a8cf8f28cc0d426a07534c7b968c3a
Gador Cruz <gadorcruz@hotmail.com>
2023-07-19 17:23:24 +0100

    create the status for the rendering

--- questionslist/src/App.js
+++ questionslist/src/App.js
@@ -1,17 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const QUESTIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/questions';
 const SUBMISSIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/submissions';
 
 export default function QuestionList() {
-  const [question, setQuestion] = useState([])
+  const [questions, setQuestions] = useState([])
+  const [submissions, setSubmissions] = useState([])
 
   useEffect(() => {
     fetch(QUESTIONS_API_BASE_URL)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setQuestion(data);
+        console.log(data[0].id);
+        setQuestions(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(SUBMISSIONS_API_BASE_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[0].id);
+        setSubmissions(data);
       })
       .catch((err) => {
         console.log(err.message);
@@ -22,16 +35,23 @@ export default function QuestionList() {
     <>
       <div class="category">
         <h2>CSS -  1 / 5</h2>
-        <div class="question">
-          <div class="status incorrect"></div>
-          <h3>{question[1].name}</h3>
+        {questions.map((question) => (
+          <div class="question">
+            <div class="status incorrect"></div>
+          <h3>{question.name}</h3>
         </div>
-        <div class="question">
-          <div class="status partially-correct"></div>
-          <h3>Navbar</h3>
-        </div>
-      </div>
+      ))}</div>
 
     </>
   );
 }
+
+
+{/* <div class="category">
+        <h2>CSS -  1 / 5</h2>
+        {questions.map((question) => (
+          <div class="question">
+            <div class="status incorrect"></div>
+          <h3>{question.name}</h3>
+        </div>
+      ))}</div> */}
