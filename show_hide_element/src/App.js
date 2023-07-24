9151a81943050f458f291b09651125fd2194a4a1
Gador Cruz <gadorcruz@hotmail.com>
2023-07-24 17:01:55 +0100

    added the button rendering

---
+++ show_hide_element/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function App() {
+
+  const [toggleActive, setToggleActive] = useState(true);
+
+  function handleClick () {
+    if (toggleActive) {
+      setToggleActive(false);
+    }else{
+      setToggleActive(true);
+    }
+
+  }
+
+  return (
+    <>
+      <button onClick= {handleClick}>Hide Element Below</button>
+
+      {toggleActive ? <div>Toggle Challenge</div> : null}
+
+    </>
+  );
+}
