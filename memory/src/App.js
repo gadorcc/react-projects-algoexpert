bbac1c6cccc4763e4470852af26b934b5840686a
Gador Cruz <gadorcruz@hotmail.com>
2023-07-26 22:16:45 +0100

    change the cell className to update colors

--- memory/src/App.js
+++ memory/src/App.js
@@ -3,22 +3,47 @@ import { useState, useEffect} from 'react';
 
 const TILE_COLORS = ['red', 'green', 'blue', 'yellow'];
 
-  function Memory() {
-  // Write your code here.
+function Memory() {
+
   const colors = shuffle(TILE_COLORS).concat(shuffle(TILE_COLORS));
-  const colorsObject = {...colors}
-  console.log(colorsObject)
-  const value = Object.entries(colorsObject).map((v,k) => <div key={k} className="tile" value={v[1]}></div>)
-  // const colorsClass = colorsObject.map((color) => <div key= {user.id}>{color.name}</div>)
+  const [cells, setCells] = useState([{id:0, color: colors[0], className: "tile"},{id:1, color: colors[1], className: "tile"},{id:2, color: colors[2], className: "tile"},{id:3, color: colors[3], className: "tile"},{id:4, color: colors[4], className: "tile"},{id:5, color: colors[5], className: "tile"},{id:6, color: colors[6], className: "tile"},{id:7, color: colors[7], className: "tile"}]);
+
+
+  const changeColor = (id) => {
+     // 1. Find the todo with the provided id
+    const currentCellIndex = cells.findIndex((cell) => cell.id === id);
+  // 2. Change the className
+    const updatedCells = Object.assign({}, cells[currentCellIndex]);
+    updatedCells.className = "tile " + updatedCells.color;
+  // 3. Update the cells
+    const newCells = cells.slice();
+    newCells[currentCellIndex] = updatedCells;
+    setCells(newCells);
+
+  }
+
+
+
+
   return (
     <>
       <h1>Memory</h1>
       <div className="board">
-        {value}
+        {cells.map((cell) => (
+          <div
+            key={cell.id}
+            className={cell.className}
+            onClick={() => changeColor(cell.id)}
+          >
+            {cell.color}
+          </div>
+        ))}
       </div>
+
     </>
   );
 }
+
 
 /**
  * Returns the array shuffled into a random order.
